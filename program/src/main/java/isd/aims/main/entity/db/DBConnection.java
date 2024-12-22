569f418e685f91ec3f5b18bd4000a9ef2e21931d
namkhanh03 <tnkhanhs03@gmail.com>
2024-12-22 16:30:13 +0700

    update filter, sort and search product

--- program/src/main/java/isd/aims/main/entity/db/DBConnection.java
+++ program/src/main/java/isd/aims/main/entity/db/DBConnection.java
@@ -9,28 +9,14 @@ import isd.aims.main.utils.Utils;
 
 public class DBConnection {
 
-    private static Logger LOGGER = Utils.getLogger(Connection.class.getName());
-    private static Connection connect;
+	private static Logger LOGGER = Utils.getLogger(Connection.class.getName());
+	private static Connection connect;
 
     public static Connection getConnection() {
         if (connect != null) return connect;
 
         try {
-//            String url = "jdbc:mysql://localhost:3306/aims";
-//            String username = "root";
-//            String password = "";
-
-            // DB cua hung
-            String url = "jdbc:mysql://localhost:3306/aims";
-            String username = "root";
-            String password = "";
-
-            connect = DriverManager.getConnection(url,username,password);
-            Statement statement = connect.createStatement();
-            ResultSet resultSet = statement.executeQuery("select * from cd");
-            while (resultSet.next()){
-                System.out.println("Yes");
-            }
+            connect = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/aims","root","motconvit123");
             return connect;
         } catch (Exception e) {
             LOGGER.info(e.getMessage());
